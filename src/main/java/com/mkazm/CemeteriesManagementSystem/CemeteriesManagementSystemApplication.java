f6563ab382e1c0604561fd1adabccd4fa095e946
Michał Kaźmierczak <mkazmierczak112@gmail.com>
2023-01-08 21:37:06 +0100

    Init commit

---
+++ src/main/java/com/mkazm/CemeteriesManagementSystem/CemeteriesManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mkazm.CemeteriesManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemeteriesManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CemeteriesManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkazm/CemeteriesManagementSystem/CemeteriesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkazm.CemeteriesManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CemeteriesManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
